ccecb7a9246eea261b222a22f1a6fe063f477e4a
Yasmine Abulhab <yabulhab@gmail.com>
2016-04-16 21:13:34 -0400

    THIS IS AWESOME

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
 
+//initiates the angular app
+var toDoApp = angular.module('toDoApp', []);
 
 console.log("I'm working!");
+
+toDoApp.controller("ToDoListCtrl", function($scope) {
+
+	$scope.toDoListName = "Yasmine's ToDo List" ;
+	$scope.myInputText= "";
+	$scope.toDo = [
+		{text:"fold laundry", completed:false}, 
+		{text:"forage for meals", completed:false}
+	];
+
+
+
+});
